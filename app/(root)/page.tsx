075029ca58450459ce4608756ed70a8624aad0c9
chemist-god <godtenkorang72@gmail.com>
2024-12-16 17:22:12 +0000

    modify page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import Hello from "../components/hello";
+
 
 export default function Home() {
   
 
   return (
     <>
-    <h1 className="text-3xl"> Welcome To Nextjs</h1>
-    <Hello/>
+   <h1 className="text-2xl">Home</h1>
     </>
   );
 }
