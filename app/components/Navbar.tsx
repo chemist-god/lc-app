05fc9e659c16574e9dc44a65daf547518911b2f6
chemist-god <godtenkorang72@gmail.com>
2024-12-16 11:57:24 +0000

    modoified navbar for clean code

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,40 +1,41 @@
-import Image from 'next/image'
-import Link  from "next/link"
-import { auth, signOut } from '@/auth'
+import Image from 'next/image';
+import Link from "next/link";
+import { auth, signIn, signOut } from '@/auth';
 
-const Navbar = async() => {
-    const session = await auth()
+const Navbar = async () => {
+    const session = await auth();
 
-
-  return (
-    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-     <nav className='flex justify-betwee items-center'>
-        <Link href="/">
-        <Image src="/logo.png" alt="logo" width={144} height={30} />
-        </Link>
-
-        <div className='flex items-center gap-5'>
-            {session && session?.user ? (
-                <>
-                <Link href="/learn/create">
-                <span>Create</span>
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href="/">
+                    <Image src="/logo.png" alt="logo" width={144} height={30} />
                 </Link>
 
-                <button onClick={signOut}>
-                    <span>Logout</span>
-                </button>
+                <div className='flex items-center gap-5'>
+                    {session?.user ? (
+                        <>
+                            <Link href="/learn/create">
+                                <span>Create</span>
+                            </Link>
 
-                    <Link href={'/user/${session ?.id}'}>
-                        <span>{session?.user?.name}</span>
+                            <button onClick={signOut} aria-label="Logout">
+                                <span>Logout</span>
+                            </button>
 
-                    </Link>
-
-                </>
-            )}
-        </div>
-     </nav>
-    </header>
-  )
+                            <Link href={`/user/${session.id}`}>
+                                <span>{session.user.name}</span>
+                            </Link>
+                        </>
+                    ) : (
+                        <button onClick={() => signIn({ provider: 'github' })} aria-label="Login">
+                            <span>Login</span>
+                        </button>
+                    )}
+                </div>
+            </nav>
+        </header>
+    );
 }
 
-export default Navbar
+export default Navbar;
