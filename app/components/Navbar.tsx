ed6c5c0e6411c35a8f61a5755eb7e426ae91848b
chemist-god <godtenkorang72@gmail.com>
2024-12-16 11:49:01 +0000

    modified Navbar to check if user is loged in

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import React from 'react'
 import Link  from "next/link"
+import { auth, signOut } from '@/auth'
 
-const Navbar = () => {
+const Navbar = async() => {
+    const session = await auth()
 
-    
+
   return (
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
      <nav className='flex justify-betwee items-center'>
@@ -13,7 +14,23 @@ const Navbar = () => {
         </Link>
 
         <div className='flex items-center gap-5'>
+            {session && session?.user ? (
+                <>
+                <Link href="/learn/create">
+                <span>Create</span>
+                </Link>
 
+                <button onClick={signOut}>
+                    <span>Logout</span>
+                </button>
+
+                    <Link href={'/user/${session ?.id}'}>
+                        <span>{session?.user?.name}</span>
+
+                    </Link>
+
+                </>
+            )}
         </div>
      </nav>
     </header>
