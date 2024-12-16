b561825c29019377aea291be8c8121d4ef948921
chemist-god <godtenkorang72@gmail.com>
2024-12-16 17:18:32 +0000

    modified Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,41 +1,7 @@
-import Image from 'next/image';
-import Link from "next/link";
-import { auth, signIn, signOut } from '@/auth';
 
-const Navbar = async () => {
-    const session = await auth();
-
+const Navbar = () => {
     return (
-        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-            <nav className='flex justify-between items-center'>
-                <Link href="/">
-                    <Image src="/logo.png" alt="logo" width={144} height={30} />
-                </Link>
-
-                <div className='flex items-center gap-5'>
-                    {session?.user ? (
-                        <>
-                            <Link href="/learn/create">
-                                <span>Create</span>
-                            </Link>
-
-                            <button onClick={signOut} aria-label="Logout">
-                                <span>Logout</span>
-                            </button>
-
-                            <Link href={`/user/${session.id}`}>
-                                <span>{session.user.name}</span>
-                            </Link>
-                        </>
-                    ) : (
-                        <button onClick={() => signIn({ provider: 'github' })} aria-label="Login">
-                            <span>Login</span>
-                        </button>
-                    )}
-                </div>
-            </nav>
-        </header>
-    );
-}
+        <div className=""></div>
+};
 
 export default Navbar;
