82ca6d70f732883caae766df92943f47f47c634a
chemist-god <godtenkorang72@gmail.com>
2024-12-16 10:12:58 +0000

    add api route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Hello from "../components/hello";
 
 export default function Home() {
-  console.log('what am i doing here?  == SERVER/CLIENT');
+  
 
   return (
     <>
