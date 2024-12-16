ced00da24f9d5f331c83d936677db39a44aad658
chemist-god <godtenkorang72@gmail.com>
2024-12-16 11:31:05 +0000

    includ logo in  navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import Link  from "next/link"
+
+const Navbar = () => {
+
+    
+  return (
+    <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+     <nav className='flex justify-betwee items-center'>
+        <Link href="/">
+        <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className='flex items-center gap-5'>
+
+        </div>
+     </nav>
+    </header>
+  )
+}
+
+export default Navbar
