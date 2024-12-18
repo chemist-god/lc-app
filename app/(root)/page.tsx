5a1b154fe11b774db5739aa9fbd4bc97385c1e84
chemist-god <godtenkorang72@gmail.com>
2024-12-18 10:41:43 +0000

    included searchParams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,10 @@ import SearchForm from "../components/SearchForm";
 
 
 
-export default function Home() {
-  
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{query?: string}>
+}) {
+    const query = (await searchParams).query;
 
   return (
     <>
@@ -15,7 +17,7 @@ export default function Home() {
    Ask questions, receive feedback, and test your knowledge through virtual competitions.
    </p>
     
-   <SearchForm/>
+   <SearchForm query={query}/>
 
     </section>
     </>
