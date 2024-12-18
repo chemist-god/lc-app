2688bb49f5de1b1d2466daf96de93dd04f1d87dc
chemist-god <godtenkorang72@gmail.com>
2024-12-18 19:50:55 +0000

    included learn detail button

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,9 +3,10 @@ import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 const StartupCard = ({post}: { post: StartupTypeCard}) => {
-    const { _createdAt, views, author:{id: authorId, name}, title, category, _id, image } = post;
+    const { _createdAt, views, author:{id: authorId, name}, title, category, _id, image, description } = post;
 
   return (
     <li className='startup-card group'>
@@ -32,6 +33,30 @@ const StartupCard = ({post}: { post: StartupTypeCard}) => {
                 <Image src= "https://placehold.co/48x48" alt= "placeholder" width={48} height={48} className='rounded-full'/>
              </Link>
         </div>
+        <Link href={`/learn/${_id}`}>
+  <p className="startup-card_desc">{description}</p>
+  <Image
+    src={image}
+    alt="placeholder"
+    width={400} 
+    height={500} 
+    className="startup-card_img"
+    priority // Optional: Ensures faster loading for critical images
+  />
+</Link>
+
+    <div className='flex-between gap-3 mt-5'>
+        <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className='text-16-medium'>
+                {category}
+            </p>
+        </Link>
+        <Button className='startup-card_btn' asChild>
+            <Link href={`/learn/${_id}`}>
+                Learn
+            </Link>
+        </Button>
+    </div>
     </li>
   )
 }
