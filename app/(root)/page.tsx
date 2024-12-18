4421378d4db3d542583e16ffa88c08c45c13733c
chemist-god <godtenkorang72@gmail.com>
2024-12-18 02:36:49 +0000

    building up the hero page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
 
   return (
     <>
-   <h1 className="text-2xl">Home</h1>
+
+    <section className="pink_container">
+   <h1 className="heading">Home</h1>
+    </section>
     </>
   );
 }
