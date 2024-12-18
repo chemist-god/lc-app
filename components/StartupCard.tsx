cfb6b15d428849242a0d1518523c982924dc820e
chemist-god <godtenkorang72@gmail.com>
2024-12-18 15:30:14 +0000

    imported formatDate module and passed it in the post._createdAt

---
+++ components/StartupCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { formatDate } from '@/lib/utils'
+
+const StartupCard = ({post}: { post: StartupTypeCard}) => {
+  return (
+    <li className='startup-card group'>
+        <div className='flex-between'>
+            <p className='startup_card_date'>
+                {formatDate(post._createdAt)}
+            </p>
+        </div>
+
+    </li>
+  )
+}
+
+export default StartupCard
