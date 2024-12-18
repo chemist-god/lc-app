73cffa736030f06f6e1010d59dd233a7dcdef1ae
chemist-god <godtenkorang72@gmail.com>
2024-12-18 03:14:28 +0000

    added a p tag

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
     <>
 
     <section className="pink_container">
-   <h1 className="heading">Home</h1>
+   <h1 className="heading">SELECT YOUR INTEREST, <br/> learn with understanding</h1>
+
+   <p className="sub-heading !max-w-3xl">
+   Ask questions, receive feedback, and test your knowledge through virtual competitions.
+   </p>
     </section>
     </>
   );
