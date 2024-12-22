4bb06a4ee90d4e93046d96f5b63fc3883d59ae9b
chemist-god <godtenkorang72@gmail.com>
2024-12-22 23:40:46 +0000

    Added startup.ts and migrated author.ts code

---
+++ sanity/schemaTypes/startup.ts
@@ -0,0 +1,40 @@
+import { UserIcon } from "@sanity/icons";
+import { defineField, defineType } from "sanity";
+
+export const author = defineType({
+    name: "author",
+    title: 'Author',
+    type: "document",
+    fields: [
+        defineField({
+            name: 'id',
+            type: 'string', // Consider using 'string' or 'slug' for unique identifiers
+        }),
+        defineField({
+            name: 'name',
+            type: 'string',
+        }),
+        defineField({
+            name: 'username', // Corrected typo
+            type: 'string',
+        }),
+        defineField({
+            name: 'email',
+            type: 'string',
+            validation: (Rule) => Rule.email() // Optional: Add email validation
+        }),
+        defineField({
+            name: 'image',
+            type: 'url',
+        }),
+        defineField({
+            name: 'bio',
+            type: 'text',
+        }),
+    ],
+    preview: {
+        select: {
+            title: 'name',
+        }
+    }
+});
