22e03923337410b6f5d70cbd836c6e6654ab1187
chemist-god <godtenkorang72@gmail.com>
2024-12-31 19:46:41 +0000

    add card_id:11

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -112,6 +112,16 @@ export default async function Home({
       category: "Managment",
       title: "Product Managment",
     },
+    {
+      _createdAt: new Date().toISOString(),
+      views: 88,
+      author: { _id: 11, name: 'Daniel Korsina' },
+      _id: 11,
+      description: "Cyber Security ",
+      image: "https://th.bing.com/th/id/OIP.2f9JbTuE2qGa3dj851R3KAHaD3?pid=ImgDet&w=184&h=96&c=7&dpr=1.3",
+      category: "Security",
+      title: "Cyber Security",
+    },
   ];
 
   return (
